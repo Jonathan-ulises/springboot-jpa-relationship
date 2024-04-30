a03ba02da0929ac683d3a4fd7c6628f3e865284c
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-04-30 12:04:35 -0600

    Correccion en generico de entidad de CrudRepository en Invoice

--- src/main/java/com/jonathan/curso/springboot/jpa/springbootjparelationship/repositories/InvoiceRepository.java
+++ src/main/java/com/jonathan/curso/springboot/jpa/springbootjparelationship/repositories/InvoiceRepository.java
@@ -3,7 +3,8 @@ package com.jonathan.curso.springboot.jpa.springbootjparelationship.repositories
 import org.springframework.data.repository.CrudRepository;
 
 import com.jonathan.curso.springboot.jpa.springbootjparelationship.entities.Client;
+import com.jonathan.curso.springboot.jpa.springbootjparelationship.entities.Invoice;
 
-public interface InvoiceRepository extends CrudRepository<Client, Long> {
+public interface InvoiceRepository extends CrudRepository<Invoice, Long> {
 
 }
