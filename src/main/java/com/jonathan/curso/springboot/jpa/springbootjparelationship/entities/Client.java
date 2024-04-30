0ca88a99583bc06443a61898660823a40f0e0c3f
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-04-30 13:31:40 -0600

    La relacion OneToMany

--- src/main/java/com/jonathan/curso/springboot/jpa/springbootjparelationship/entities/Client.java
+++ src/main/java/com/jonathan/curso/springboot/jpa/springbootjparelationship/entities/Client.java
@@ -1,9 +1,14 @@
 package com.jonathan.curso.springboot.jpa.springbootjparelationship.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,12 +21,17 @@ public class Client {
 
     private String name;
     private String lastname;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> adresses;
     
 
     public Client() {
+        this.adresses = new ArrayList<>();
     }
 
     public Client(String name, String lastname) {
+        this();
         this.name = name;
         this.lastname = lastname;
     }
@@ -45,4 +55,19 @@ public class Client {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
+    public List<Address> getAdresses() {
+        return adresses;
+    }
+
+    public void setAdresses(List<Address> adresses) {
+        this.adresses = adresses;
+    }
+
+    @Override
+    public String toString() {
+        return "{id=" + id + ", name=" + name + ", lastname=" + lastname + ", adresses=" + adresses + "}";
+    }
+
+    
 }
