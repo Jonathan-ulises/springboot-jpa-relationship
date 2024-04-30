906b60c6572cd6d51d54c1a4fdcaa93d43fed698
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-04-30 12:05:09 -0600

    Codificando ejemplo ManyToOne

--- src/main/java/com/jonathan/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/jonathan/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
@@ -1,13 +1,44 @@
 package com.jonathan.curso.springboot.jpa.springbootjparelationship;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jonathan.curso.springboot.jpa.springbootjparelationship.entities.Client;
+import com.jonathan.curso.springboot.jpa.springbootjparelationship.entities.Invoice;
+import com.jonathan.curso.springboot.jpa.springbootjparelationship.repositories.ClientRepository;
+import com.jonathan.curso.springboot.jpa.springbootjparelationship.repositories.InvoiceRepository;
 
 @SpringBootApplication
-public class SpringbootJpaRelationshipApplication {
+public class SpringbootJpaRelationshipApplication implements  CommandLineRunner {
+
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private InvoiceRepository invoiceRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		manyToOne();
+	}
+
+	@Transactional
+	public void manyToOne() {
+
+		Client client = new Client("John", "Doe");
+		clientRepository.save(client);
+
+		Invoice invoice = new Invoice("Compras de oficina", 2000L);
+		invoice.setClient(client);
+		Invoice invoiceDb = invoiceRepository.save(invoice);
+		System.out.println(invoiceDb);
+	}
 }
